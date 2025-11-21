818c5319a6e18222d8e073211f40a878d6430169
pretorius360 <pretorius360@gmail.com>
2025-11-21 16:21:32 +0200

    Delete Mods directory

--- Mods/FullSan/Scripts/main.lua
+++
@@ -1,5 +0,0 @@
-NotifyOnNewObject("/Script/Pal.PalGameSetting", function(var)
-	var.AutoSANRegene_Percent_perSecond_PalStorage  = 100.0
-	var.FullStomachPercent_RecoverySanity  = 100
-	var.RecoverySanity_FullStomach  = 100.0
-end)
--- Mods/MapUnlocker/Scripts/main.lua
+++
@@ -1,5 +0,0 @@
---Open entire map
-NotifyOnNewObject("/Script/Pal.PalGameSetting", function(PalGameSetting)
-  PalGameSetting.worldmapUIMaskClearSize = 20000
-end)
---Open entire map
